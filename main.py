d5403bec61891acb98b4610ccabe6ab29a2a071b
mcgue <mcgu@protonmail.com>
2022-12-27 13:37:39 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Email slicer
+
+
+def get_email(email):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Your {email}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_email('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

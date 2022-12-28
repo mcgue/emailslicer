594ac1e3d3a5c9e367b246d51b33f7faf499b6b0
mcgue <mcgu@protonmail.com>
2022-12-28 10:35:56 -0800

    get email input

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Email slicer
 
 
-def get_email(email):
+def get_email():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Your {email}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    email = input('What is your email?')
+    print(f'Your email is {email}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_email('PyCharm')
+    get_email()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

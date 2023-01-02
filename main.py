677b3ed41de0489d77d66b6ae8fdfee85e57bb48
mcgue <mcgu@protonmail.com>
2023-01-02 10:44:34 -0800

    write function to get and split email into two parts

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Email slicer
 
-# Function to get and process email
+# Function to get and split email
 def process_email():
     # Get email
     email = input('What is your email?')
     # Split
-    email_split = email.split('@',1)
-    print(f'Your email is {email_split}')
+    email_split = email.split('@', 1)
+    print(f'Your username is {email_split[0]}')
+    print(f'Your domain is {email_split[1]}')
 
-# run if main
+# Run if main
 if __name__ == '__main__':
     process_email()

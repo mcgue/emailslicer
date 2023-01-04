4c1d4a27cc1851dd32232b25ce65159dcaad7f63
mcgue <mcgu@protonmail.com>
2023-01-03 16:43:18 -0800

    finish comments

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# Email slicer
+# Email slicer to split email into username and domain
 
-# Function to get and split email
+# Function gets and splits email into username and domain
 def process_email():
     # Get email
     email = input('What is your email?')
-    # Split
+    # Split email into username and domain
     email_split = email.split('@', 1)
+    # Print username and domain separately
     print(f'Your username is {email_split[0]}')
     print(f'Your domain is {email_split[1]}')
 
 # Run if main
 if __name__ == '__main__':
+    # Run function to split email into username and domain
     process_email()
